6ca44c1a14dd087cbc4327dcf8a3bad3f2ceb7f5
kolnn <126846715+kolnn@users.noreply.github.com>
2023-03-27 16:16:14 +0900

    Update hw.3.py

--- hw.3.py
+++ hw.3.py
@@ -1,13 +1,9 @@
-def get_fixed_price(prompt) :
-    d= int(input(prompt))
-    a= int(input(ana))
-    b= int(input(anb))
-    pa= (100*a)/(100 - d)
-    print('A 상품의 정가는 ', pa, '원')
-    pb= (100*b)/(100 - d)
-    print('B 상품의 정가는 ', pb, '원')
+import get_fixed_price as f
 
-prompt='할인율은? '
-ana='A 상품의 할인된 가격은? '
-anb='B 상품의 할인된 가격은? '
-get_fixed_price(prompt)
+per = int(input("할인율은? "))
+f.get_fixed_price(per)
+
+A = int(input('A 상품의 할인된 가격은?'))
+B = int(input("B 상품의 할인된 가격은?"))
+print('A 상품의 정가는',f.A(A),'원')
+print('B 상품의 정가는',f.B(B),'원')
